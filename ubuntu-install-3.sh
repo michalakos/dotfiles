f97203f3dcb958665565c5233c9f44fafc657502
michalakos <michalakos@workdesk.station>
2021-12-15 13:40:23 +0200

    Add app installation script

---
+++ ubuntu-install-3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+cd ~
+
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+# install discord
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi ~/discord.deb
+# install vscode
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+# install obs
+$ sudo apt install obs-studio
+# install spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+# install texmaker
+sudo apt install texlive-full
+sudo apt install texmaker
+# install vlc
+sudo apt install vlc
